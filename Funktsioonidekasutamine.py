174619f69a0250edbe9e166e2dda891d7f1cd3bb
opilane <opilane@HP-CZC2349HV2>
2024-02-21 11:42:58 +0200

    Add project files.

---
+++ Funktsioonidekasutamine.py
@@ -0,0 +1,12 @@
+#from MyModul import * #Summa as s
+#b=int(input("Sisesta arv2"))
+#summa_3=Summa(3,b,int(input("Kolmas arv: ")))
+#summa_31=Summa(100,100)
+
+#print(summa_3)
+#print(summa_31)
+
+from MyModul import *
+v=input()
+i=Clasificator(v)
+
---
+++ ViblyyFunctions.py
@@ -0,0 +1,44 @@
+﻿#def Summa(arv1:int,arv2:int,arv3=0)->int:
+#    """ Funktsioon tagastab kolme arvu summa
+#    Summa(arv1,arv2,arv3)
+    
+#    :param int arv1: Arv1 sisestab kasutaja
+#    :param int arv2: Arv1 sisestab kasutaja
+#    :param int arv3: Vaikimisi ta võrdub nulliga
+#    """
+#    s=arv1+arv2+arv3
+#    return s
+
+
+#def Clasificator(x)->any:
+#    if type(x)==str:
+#        s=print(x," on text")
+#    if type(x)==int:
+#        s=print(x," on täisarv")
+
+#    if type(x)==str:
+#        s=print(x," on float")
+#    return s
+
+
+#1
+#def Arithmetica(arv1:float,arv2:float,operatsioon)->float:
+    
+#    if operatsioon=="+":
+#        return arv1+arv2  
+        
+#    elif operatsioon=="-":
+#        return arv1-arv2 
+        
+        
+#    elif operatsioon=="*":
+#        return arv1*arv2 
+#    elif operatsioon=="/":
+#        if not(arv2==0):
+#            return  arv1/arv2 
+#        else:
+#            return "Nulliga jagada ei saa."
+        
+
+#    else:
+#        print("Vale operatsioon")
